92ade198d5c92ef54644f4e0202334b8d69bbdfd
Luis Novais <novais.me@gmail.com>
2014-02-19 21:44:25 +0000

    Criação de projeto para ser suite de programação. Inicialmente num versão muito base

---
+++ MyProgrammerSuite/Components/Entities.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyProgrammerSuite.Components
+{
+    public class Entities
+    {
+        private List<Entity> _entitiesList = new List<Entity>();
+
+
+        public List<Entity> EntitiesList
+        {
+            get { return _entitiesList; }
+            set { _entitiesList = value; }
+        }
+
+        public Entities()
+        {
+        }
+    }
+}
---
+++ MyProgrammerSuite/Components/Entity.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyProgrammerSuite.Components
+{
+    public class Entity
+    {
+        public String Value { get; set; }
+
+        public String Translation { get; set; }
+
+        public Entity(string value, string translation)
+        {
+            Value = value;
+            Translation = translation;
+        }
+    }
+}
