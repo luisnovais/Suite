fc6a24e07dc4ac0b9b09f0c4a9ec64bfccccc46e
luisnovais <novais.me@gmail.com>
2014-04-07 11:41:45 +0100

    ConsoleApplication para avaliar Expressões regulares

---
+++ ConsoleTester/Components/RegexBuilder.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace ConsoleTester.Components
+{
+    public static class RegexBuilder
+    {
+        public static void TestRegex(string value)
+        {
+            var valueToEvaluate = value.ToLower().Replace(" ","");
+
+            Match match = Regex.Match(valueToEvaluate, @"^[\w0-9,-.]+$");
+
+            //Match match = Regex.Match(valueToEvaluate, @"^[\w*0-9,.-]+$/ix");
+            if (match.Success)
+            {
+                Console.WriteLine("Foram encontradas matchs");
+                Console.WriteLine("Valor Valido");
+                
+            }else
+            {
+                Console.WriteLine("Valor invalido");
+            }
+        }
+    }
+}
---
+++ ConsoleTester/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using ConsoleTester.Components;
+
+namespace ConsoleTester
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            string valor = "àÁca-nçãod_ASSDASDASD [ASDsdasdsdsd.,,-asfasfsdf";
+            RegexBuilder.TestRegex(valor);
+            Console.ReadKey();
+        }
+    }
+}
